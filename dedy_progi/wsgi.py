3a443d57fecfb9fe18ca5443d2376114e61847fd
katepu <95654025+katepu@users.noreply.github.com>
2023-04-30 14:29:38 +0100

    meh

--- dedy_progi/wsgi.py
+++ dedy_progi/wsgi.py
@@ -1,13 +1,9 @@
-"""
-WSGI config for dedy_progi project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
